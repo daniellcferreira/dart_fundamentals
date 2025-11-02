13e76f656acdc34f2249329b2e2adacdb55b9d76
Daniel Ferreira <dcfer2017@gmail.com>
2025-11-02 12:17:09 -0300

    feat: adiciona entrada de nome via terminal e mensagem personalizada

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
 void main() {
-  print('Hello, Meu App!');
+  print("Olá me chamo Dart. Qual é o seu nome?");
+  var entrada = stdin.readLineSync();
+  print("Prazer em te conhecer, $entrada!");
 }
