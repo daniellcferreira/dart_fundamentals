c336b36f23efbb69a2e7be5afdb148685ce3605a
Daniel Ferreira <dcfer2017@gmail.com>
2025-11-22 09:45:34 -0300

    feat: adicionar classe Conta e primeira implementação de lista de contas

--- anybank/bin/anybank.dart
+++ anybank/bin/anybank.dart
@@ -1,5 +1,13 @@
 import 'package:anybank/anybank.dart' as anybank;
 
-void main(List<String> arguments) {
-  print('Hello world: ${anybank.calculate()}!');
+void main() {
+  List<String> contas = <String>['Conta1', 'Conta2', 'Conta3'];
+  List<double> saldos = <double>[1000.0, 2500.5, 300.75];
+}
+
+class Conta {
+  String titular;
+  double saldo;
+
+  Conta(this.titular, this.saldo);
 }
