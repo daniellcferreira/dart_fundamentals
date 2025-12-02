7b5f708e9b01061746e3fbc7478c62714ae91128
Daniel Ferreira <dcfer2017@gmail.com>
2025-12-02 11:30:15 -0300

    feat: adicionar função requestData() com requisição HTTP usando package:http

--- dart_assincronismo/bin/main.dart
+++ dart_assincronismo/bin/main.dart
@@ -1,3 +1,12 @@
+import 'package:http/http.dart';
+
 void main() {
-  print('Hello, Dart!');
+  // print('Hello, Dart!');
+  requestData();
+}
+
+requestData() {
+  String url =
+      "https://gist.githubusercontent.com/daniellcferreira/ddcbf1eeadd0b69bead25a1b158f2091/raw/fc8a14b9cb47622fa71c7801757940662943d02d/accounts.json";
+  print(get(Uri.parse(url)));
 }
