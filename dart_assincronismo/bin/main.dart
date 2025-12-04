8a3fb31610d0ddc0be254353e92fdf64dfde26de
Daniel Ferreira <dcfer2017@gmail.com>
2025-12-04 11:22:09 -0300

    feat: implementar requisição HTTP usando Future e then() em requestData()

--- dart_assincronismo/bin/main.dart
+++ dart_assincronismo/bin/main.dart
@@ -8,5 +8,9 @@ void main() {
 requestData() {
   String url =
       "https://gist.githubusercontent.com/daniellcferreira/ddcbf1eeadd0b69bead25a1b158f2091/raw/fc8a14b9cb47622fa71c7801757940662943d02d/accounts.json";
-  print(get(Uri.parse(url)));
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
+  futureResponse.then((Response response) {
+    print(response.body);
+  });
 }
