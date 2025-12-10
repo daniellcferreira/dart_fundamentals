ec78dc9fbff509755b23d7bc384212a3fa71e2c7
Daniel Ferreira <dcfer2017@gmail.com>
2025-12-10 09:59:37 -0300

    feat: adicionar exemplo principal com criação de contas e transferência bancária

--- dart_exceptions/bin/dart_exceptions.dart
+++ dart_exceptions/bin/dart_exceptions.dart
@@ -1,5 +1,26 @@
-import 'package:dart_exceptions/dart_exceptions.dart' as dart_exceptions;
+import 'controllers/bank_controller.dart';
+import 'models/account.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_exceptions.calculate()}!');
+void main() {
+  // Criando o banco
+  BankController bankController = BankController();
+
+  // Criando contas
+  bankController.addAccount(
+    id: "Pedro",
+    account: Account(name: "Pedro Alves", balance: 500, isAuthenticated: true),
+  );
+  bankController.addAccount(
+    id: "Maria",
+    account: Account(name: "Maria Silva", balance: 300, isAuthenticated: true),
+  );
+
+  // Fazendo trasnferências
+  bool result = bankController.makeTransfer(
+    idSender: "Pedro",
+    idReceiver: "Maria",
+    amount: 200,
+  );
+
+  print("Transferência realizada com sucesso: $result");
 }
