a5b6f150052d1d231606b45f0a2078dfd96fdb3c
Daniel Ferreira <dcfer2017@gmail.com>
2025-12-15 10:09:22 -0300

    feat: adicionar tratamento de exceções no fluxo principal de transferência

--- dart_exceptions/bin/dart_exceptions.dart
+++ dart_exceptions/bin/dart_exceptions.dart
@@ -1,26 +1,51 @@
 import 'controllers/bank_controller.dart';
+import 'exceptions/bank_controller_exceptions.dart';
 import 'models/account.dart';
 
 void main() {
+  //assert(3 > 4);
+  //assert(3 > 4, "Que pena, 3 não é maior que 4");
+
   // Criando o banco
   BankController bankController = BankController();
 
-  // Criando contas
+  Account testAccount = Account(name: "", balance: 0, isAuthenticated: true);
+
+  // Adicionando contas
   bankController.addAccount(
-    id: "Pedro",
-    account: Account(name: "Pedro Alves", balance: 500, isAuthenticated: true),
-  );
-  bankController.addAccount(
-    id: "Maria",
-    account: Account(name: "Maria Silva", balance: 300, isAuthenticated: true),
+    id: "Ricarth",
+    account: Account(
+      name: "Ricarth Lima",
+      balance: 400,
+      isAuthenticated: false,
+    ),
   );
 
-  // Fazendo trasnferências
-  bool result = bankController.makeTransfer(
-    idSender: "Pedro",
-    idReceiver: "Maria",
-    amount: 200,
+  bankController.addAccount(
+    id: "Kako",
+    account: Account(name: "Caio Couto", balance: 600, isAuthenticated: true),
   );
 
-  print("Transferência realizada com sucesso: $result");
+  // Fazendo transferência
+  try {
+    bankController.makeTransfer(
+      idSender: "Kako",
+      idReceiver: "Ricarth",
+      amount: 200,
+    );
+
+    print("Transação concluída com sucesso");
+  } on SenderIdInvalidException catch (e) {
+    print(e.message);
+  } on ReceiverIdInvalidException catch (e) {
+    print(e);
+  } on SenderNotAuthenticatedException catch (e) {
+    print(e);
+  } on ReceiverNotAuthenticatedException catch (e) {
+    print(e);
+  } on SenderBalanceLowerThanAmountException catch (e) {
+    print(e);
+  } catch (e) {
+    print("Erro desconhecido.");
+  }
 }
