b6456834711f3920fc09a07c60c871f0f855e8e1
Daniel Ferreira <dcfer2017@gmail.com>
2025-12-20 10:23:35 -0300

    feat: Aprimora cálculo de descontos com validações e testes expandidos

--- dart_testes/lib/dart_testes.dart
+++ dart_testes/lib/dart_testes.dart
@@ -3,6 +3,10 @@ double calcularDesconto(double valor, double desconto, bool percentual) {
     throw ArgumentError('O valor do produto deve ser maior que zero.');
   }
 
+  if (desconto <= 0) {
+    throw ArgumentError('O valor do desconto não pode ser zero.');
+  }
+
   if (percentual) {
     return valor - (valor * desconto) / 100;
   } else {
--- dart_testes/test/dart_testes_test.dart
+++ dart_testes/test/dart_testes_test.dart
@@ -19,4 +19,30 @@ void main() {
   test('Calcula o valor do produto com desconto com porcentagem', () {
     expect(app.calcularDesconto(1000, 15, true), 850);
   });
+
+  test('Calcula o valor do produto com desconto zerado com porcentagem', () {
+    expect(
+      () => app.calcularDesconto(1000, 0, true),
+      throwsA(TypeMatcher<ArgumentError>()),
+    );
+  });
+
+  group('Calcula o valor do produto com desconto', () {
+    var valuesToTest = {
+      {'valor': 1000, 'desconto': 150, 'percentual': false}: 850,
+      {'valor': 1000, 'desconto': 15, 'percentual': true}: 850,
+    };
+    valuesToTest.forEach((values, expected) {
+      test('Entrada:$values Esperado:$expected', () {
+        expect(
+          app.calcularDesconto(
+            double.parse(values["valor"].toString()),
+            double.parse(values["desconto"].toString()),
+            (values["percentual"] == true),
+          ),
+          equals(expected),
+        );
+      });
+    });
+  });
 }
